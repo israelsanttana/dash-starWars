9c9b088ffe6efaa7245472bf5fb4c72da4dce3fc
israelsanttana <israelsantanasantos7@gmail.com>
2023-06-10 16:39:07 -0300

    feat: adiciona o roteamento das páginas

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ export function App() {
 
   return (
     <>
-      < Deshboard />
+      <Deshboard />
       <People />
       <Planets />
       <Ships />
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from './App.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Deshboard } from "./pages/Deshboard";
+import { People } from "./pages/People";
+import { Planets } from "./pages/Planets";
+import { Ships } from "./pages/Ships";
+import { Films } from "./pages/Films"
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Deshboard />} />
+        <Route path='/Peoole' element={<People />} />
+        <Route path='/planets' element={<Planets />} />
+        <Route path='/ships' element={<Ships />} />
+        <Route path='/films' element={< Films />} />
+      </Routes>
+    </BrowserRouter>
+
   </React.StrictMode>,
 )
