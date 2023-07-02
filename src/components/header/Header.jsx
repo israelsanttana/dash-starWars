ca9898532a4e085b750abf17fbd40652d951be95
israelsanttana <israelsantanasantos7@gmail.com>
2023-07-01 22:32:00 -0300

    feat: adiciona logo no header mobile

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,15 +1,22 @@
 import styles from './header.module.css'
 import { List, X } from '@phosphor-icons/react'
+import iconStarWars from '../../assets/iconStarWars.png'
+
 
 export function Header({ toggleMenu, isNavOpen }) {
 
     return (
 
         <div className={styles.content}>
-            <div className={styles.contentButton}>
-                {isNavOpen ? <button onClick={toggleMenu}  ><X size={32} /></button> : <button onClick={toggleMenu}  ><List size={32} /></button>}
 
+            <div className={styles.contentButton}>
+                <div>
+                    <img src={iconStarWars} width={70} />
+                </div>
+
+                {isNavOpen ? <button onClick={toggleMenu}  ><X size={32} /></button> : <button onClick={toggleMenu}  ><List size={32} /></button>}
             </div>
+
 
         </div>
 
